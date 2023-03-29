3b420896fc9b9e50db32f539b2097f338bdf17b8
JustOverFR <jeanjuste.lefebvre@gmail.com>
2023-03-29 10:43:05 +0200

    fixed css

--- chat_serveur_client/src/main/java/com/example/chat_serveur_client/Controller.java
+++ chat_serveur_client/src/main/java/com/example/chat_serveur_client/Controller.java
@@ -68,8 +68,8 @@ public class Controller implements Initializable {
                     Text text = new Text(messageToSend);
                     TextFlow textFlow = new TextFlow(text);
 
-                    textFlow.setStyle("-fx-color: rgb(239,242,255)" +
-                            "-fx-background-color: rgb(15,125,242)" +
+                    textFlow.setStyle("-fx-color: rgb(239,242,255);" +
+                            "-fx-background-color: rgb(15,125,242);" +
                             "-fx-background-radius: 20px");
 
                     textFlow.setPadding(new Insets(5,10,5,10));
@@ -96,8 +96,8 @@ public class Controller implements Initializable {
 
         Text text = new Text(messageFromClient);
         TextFlow textFlow = new TextFlow(text);
-        textFlow.setStyle("-fx-background-color: rgb(233,233,235)"+
-                " -fx-backgroung-radius: 20px");
+        textFlow.setStyle("-fx-background-color: rgb(233,233,235);"+
+                "-fx-backgroung-radius: 20px");
 
         textFlow.setPadding(new Insets(5, 10, 5, 10));
         hBox.getChildren().add(textFlow);
